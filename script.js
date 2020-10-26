7f937c45e5a548c045b1f43cfea5595135aacf17
Val <vdecyatnik@gmail.com>
2020-10-25 20:45:47 -0700

    pseudo code

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton=document.addEventListener("click", function)
+
+
+
+
+//WHEN user clicks the start button 
+//THEN a timer starts and questions begin
+//WHEN I answer a question 
+//THEN I am presented with another question
+//WHEN I answer incorrectly 
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN i can save my initals and score.
