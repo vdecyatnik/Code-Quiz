7d296d3972912bc96091106ec1a38fdfb9bf57ee
Val <vdecyatnik@gmail.com>
2020-10-27 21:41:09 -0700

    psuedo

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
-var startButton=document.addEventListener("click", function)
+let questions = [
+  question1 = {
+    ques: "Where is Valentina from?",
+    correctAnsw: "Chicago",
+    answArray: ["San Diego", "London", "Chicago", "New York"]
+  },
+  question2 = {
+    ques: "Where is Valentina from?",
+    correctAnsw: "Chicago",
+    answArray: ["San Diego", "London", "Chicago", "New York"]
+  },
+  question3 = {
+    ques: "Where is Valentina from?",
+    correctAnsw: "Chicago",
+    answArray: ["San Diego", "London", "Chicago", "New York"]
+  },
+  question4 = {
+    ques: "Where is Valentina from?",
+    correctAnsw: "Chicago",
+    answArray: ["San Diego", "London", "Chicago", "New York"]
+  },
+  question5 = {
+    ques: "Where is Valentina from?",
+    correctAnsw: "Chicago",
+    answArray: ["San Diego", "London", "Chicago", "New York"]
+  }
+]
+
+
+
+
+
+
+
 
 
 
 
 //WHEN user clicks the start button 
+
+//function startGame(){
+
+
+    //starButton'.addEventListener("click", function);
+
+
 //THEN a timer starts and questions begin
+//setInterval set starting score
+//start interval to begin the score count down
+
 //WHEN I answer a question 
 //THEN I am presented with another question
 //WHEN I answer incorrectly 
@@ -13,3 +56,16 @@ var startButton=document.addEventListener("click", function)
 //THEN the game is over
 //WHEN the game is over
 //THEN i can save my initals and score.
+
+
+//VAR pointer/index- Current position in the question array
+//set interval
+//clear interval
+//currentScore/timeLeft
+//VAR question -Array
+// var question = [
+  //render one question to the page at once
+    //Question text
+    //list of question answers-subarray
+    //Correct Answer /index exact value etc,.
+    
