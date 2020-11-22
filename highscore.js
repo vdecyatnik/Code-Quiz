534da80986faba4ebe3d2dc81d60c4342f19f45a
Val <vdecyatnik@gmail.com>
2020-11-22 12:30:19 -0800

    added dynamic score list

--- highscore.js
+++ highscore.js
@@ -14,29 +14,52 @@ showScoresArray = JSON.parse(localStorage.getItem(highScoreKey));
 
 
 
+var showScoresList = document.getElementById("initial-scoreList");
 
+function renderLastInitials(){
 
+  var showScores = JSON.parse(localStorage.getItem(highScoreKey));
 
+  console.log(showScores);
 
-
-
-function renderLastInitials() {
-  var unOrderList = document.createElement("ul");
-  console.log(unOrderList);
-  for (var i = 0; i < showScoresArray.length; i++) {
-
+  for (i=0; i < showScores.length;i++){
     var list = document.createElement("li");
-    var span = document.createElement("span");
-    span.textContent=showScoresArray[i].score;
-    list.appendChild(span);
-    var span = document.createElement("span");
-    span.textContent=showScoresArray[i].initials;
-    list.appendChild(span);
-
-
+    list.textContent= "\nInitials: \n" + showScores[i].initials  +  "\n Score: \n" +  showScores[i].score;
+    showScoresList.append(list);
   }
 
-  document.getElementById("scorelist").appendChild(list);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// function renderLastInitials() {
+//   var unOrderList = document.createElement("ul");
+//   console.log(unOrderList);
+//   for (var i = 0; i < showScoresArray.length; i++) {
+
+//     var list = document.createElement("li");
+//     var span = document.createElement("span");
+//     span.textContent=showScoresArray[i].score;
+//     list.appendChild(span);
+//     var span = document.createElement("span");
+//     span.textContent=showScoresArray[i].initials;
+//     list.appendChild(span);
+
+
+//   }
+
+//   document.getElementById("scorelist").appendChild(list);
   
 
 
@@ -47,9 +70,16 @@ function renderLastInitials() {
 
 
 
-}
+
 
 renderLastInitials();
+
+
+
+
+
+
+
 
 
 
@@ -61,7 +91,4 @@ renderLastInitials();
 
 
 
-//}
-
-
-
+//
