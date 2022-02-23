d092e11836fc7d645908ccb1ae07b808ec608959
Val <vdecyatnik@gmail.com>
2022-02-23 15:38:24 -0800

    html

--- assets/JS/highscore.js
+++ assets/JS/highscore.js
@@ -1,116 +1,41 @@
 var scorePageScore = document.getElementById("getfinalscore");
 var scorePageInitials = document.getElementById("getinitials");
 
-
 var showScoresArray = [];
 var highScoreKey = "highScoreKey";
 
-
 showScoresArray = JSON.parse(localStorage.getItem(highScoreKey));
-
-
-
-
-
-
 
 var showScoresList = document.getElementById("initial-scoreList");
 
-function renderLastInitials(){
-
+function renderLastInitials() {
   var showScores = JSON.parse(localStorage.getItem(highScoreKey));
 
   console.log(showScores);
-    
-  showScores = showScores.sort(function(a,b){
-    return parseInt(b.score)-parseInt(a.score)
-  })
 
+  showScores = showScores.sort(function (a, b) {
+    return parseInt(b.score) - parseInt(a.score);
+  });
 
-  for (i=0; i < showScores.length;i++){
+  for (i = 0; i < showScores.length; i++) {
     var list = document.createElement("li");
-    list.textContent= "\nInitials: \n" + showScores[i].initials  +  "\n Score: \n" +  showScores[i].score;
+    list.textContent =
+      "\nInitials: \n" +
+      showScores[i].initials +
+      "\n Score: \n" +
+      showScores[i].score;
     showScoresList.append(list);
   }
-
-
 }
 
-
-
-
-
-function clearLocalStorage(){
+function clearLocalStorage() {
   var listItems = document.getElementById("initial-scoreList");
- 
+
   localStorage.removeItem("highScoreKey");
-  listItems.innerHTML = "";  
-
-  
-
-
+  listItems.innerHTML = "";
 }
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function renderLastInitials() {
-//   var unOrderList = document.createElement("ul");
-//   console.log(unOrderList);
-//   for (var i = 0; i < showScoresArray.length; i++) {
-
-//     var list = document.createElement("li");
-//     var span = document.createElement("span");
-//     span.textContent=showScoresArray[i].score;
-//     list.appendChild(span);
-//     var span = document.createElement("span");
-//     span.textContent=showScoresArray[i].initials;
-//     list.appendChild(span);
-
-
-//   }
-
-//   document.getElementById("scorelist").appendChild(list);
-  
-
-
-
-  
-  console.log(showScoresArray);
-  //console.log(HighScoreDisplayArr);
-
-
-
-
+console.log(showScoresArray);
+//console.log(HighScoreDisplayArr);
 
 renderLastInitials();
-
-
-
-
-
-
-
-
-
-
-
-
-//document.getElementById("clearScore").addEventListener("click", clearScores)
-
-//function clearScores(){
-
-
-
-//
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,50 +1,46 @@
-
 var questions = [
   {
     ques: "Which of the following is not JavaScript data types?",
     answer: "Float",
-    answrArray: ["Undefined", "Number", "Boolean", "Float"]
+    answrArray: ["Undefined", "Number", "Boolean", "Float"],
   },
 
   {
     ques: "Which company developed JavaScript?",
     answer: "Netscape",
-    answrArray: ["Netscape", 'Bell Labs', "Sun Microsystems", "IBM"]
+    answrArray: ["Netscape", "Bell Labs", "Sun Microsystems", "IBM"],
   },
 
   {
     ques: "Inside which HTML element do we put Javascript?",
     answer: "<script>",
-    answrArray: ["<script>", "<head>", "<style>", "<meta>"]
+    answrArray: ["<script>", "<head>", "<style>", "<meta>"],
   },
 
   {
     ques: "What is the original name of JavaScript?",
     answer: "Mocha",
-    answrArray: ["LiveScript", "Mocha", "EScript", "JavaScript"]
+    answrArray: ["LiveScript", "Mocha", "EScript", "JavaScript"],
   },
 
   {
-
     ques: "JavaScipt is.....?",
     answer: "object based",
-    answrArray: ["subjective", "objective", "evil", "object based"]
+    answrArray: ["subjective", "objective", "evil", "object based"],
   },
 
   {
     ques: "Which one of these is not a logical operator?",
     answer: "&",
-    answrArray: ["!", "&", "||", "&&"]
+    answrArray: ["!", "&", "||", "&&"],
   },
 
   {
     ques: "Which of the following variable types does not exist in Javascript?",
     answer: "string",
-    answrArray: ["boolean", "object", "double", "string"]
-  }
-
-]
-
+    answrArray: ["boolean", "object", "double", "string"],
+  },
+];
 
 //HTML Elements
 var landingPage = document.getElementById("landingPage");
@@ -52,11 +48,9 @@ var scorePage = document.getElementById("scorePage");
 var enterInitial = document.getElementById("enterInitial");
 var quizPage = document.getElementById("quizPage");
 
-
 var startButton = document.getElementById("startButton");
 var playAgain = document.getElementById("playAgain");
 var clearScore = document.getElementById("clearScore");
-
 
 var displayScore = document.getElementById("displayScore");
 var initials = document.getElementById("initials");
@@ -72,7 +66,6 @@ var abuttons = document.getElementById("abuttons");
 
 var answersList = document.getElementById("answersList");
 
-
 //Local Storage
 var highScoreKey = "highScoreKey";
 var highScoresArray = JSON.parse(localStorage.getItem(highScoreKey)) || [];
@@ -80,17 +73,15 @@ var highScoresArray = JSON.parse(localStorage.getItem(highScoreKey)) || [];
 //Quiz Pages Array
 var pageArray = [landingPage, quizPage, enterInitial];
 
-
 //Function to hide pages
 function hide() {
   for (let i = 0; i < pageArray.length; i++) {
     console.log(pageArray[i]);
-    if (!pageArray[i].classList.contains('hide')) {
-      pageArray[i].classList.add('hide');
+    if (!pageArray[i].classList.contains("hide")) {
+      pageArray[i].classList.add("hide");
     }
   }
 }
-
 
 var seconds = 120;
 
@@ -98,51 +89,43 @@ timer.textContent = "Time: " + seconds;
 console.log(seconds);
 
 //Add event listener to start button
-document.getElementById("startButton").addEventListener("click", startGame)
-
+document.getElementById("startButton").addEventListener("click", startGame);
 
 function startGame() {
   numberCorrect = 0;
   secondsLeft = seconds;
-  questionIndex = 0
+  questionIndex = 0;
 
   quizPage.classList.remove("hide");
   landingPage.classList.add("hide");
 
-
-
-
   nextQuestion();
 
-  timer.textContent = (seconds);
+  timer.textContent = seconds;
 
   timeInterval = setInterval(function () {
     secondsLeft--;
     timer.textContent = "Time: " + secondsLeft;
 
     if (secondsLeft === 0) {
-
       endGame();
 
       secondsLeft = seconds;
-
     }
-
-
   }, 1000);
-
-
-
 }
 
-
 var questionIndex = 0;
+console.log(questionIndex)
 
+//set question index
+//set questiondisplay textcontent to questions[questionIndex].ques
+//THEN loop through the answers and set the answer to questions[questionindex].answerArray[i]
+// Then increment the questionIndex++
 
 function nextQuestion() {
-
   if (questionIndex >= questions.length) {
-    quizPage.classList.add('hide');
+    quizPage.classList.add("hide");
     displayScore.textContent = secondsLeft;
 
     enterInitial.style.display = "block";
@@ -151,90 +134,65 @@ function nextQuestion() {
     return;
   }
 
-  questionDisplay.textContent = (questions[questionIndex].ques);
+  questionDisplay.textContent = questions[questionIndex].ques;
   console.log(questionDisplay);
-
+  
 
   for (var i = 0; i < 4; i++) {
     let answer = questions[questionIndex].answrArray[i];
     document.getElementById("a" + (i + 1)).textContent = answer;
-
   }
   questionIndex++;
 }
 
+console.log(questions[0].ques)
 
 var score = 0;
 console.log(score);
 
 answersList.addEventListener("click", function (event) {
-
-
   event.preventDefault();
   if (event.target.matches("button")) {
-
     let button = event.target;
+    console.log(button)
     let selectedAnswer = button.textContent;
+    console.log(selectedAnswer)
     if (selectedAnswer === questions[questionIndex - 1].answer) {
-      
-
       console.log(score);
-
-    }
-    else {
+    } else {
       secondsLeft -= 10;
-
     }
     nextQuestion();
-
   }
-
-})
-
-
-
+});
 
 //submit initials span
 var saveInitials = document.getElementById("initials");
 //display final score
 var lastScore = document.getElementById("displayScore");
 
-
-
-
 //submit initials button
 var submitInitials = document.getElementById("initialSaver");
 submitInitials.addEventListener("click", function (e) {
-
-
   initials = document.getElementById("initials").value;
   console.log("initialshere");
   console.log(initials);
-  highScoresArray.push({ score:secondsLeft, initials:initials });
+  highScoresArray.push({ score: secondsLeft, initials: initials });
   console.log(highScoresArray);
   highScoreString = JSON.stringify(highScoresArray);
 
   localStorage.setItem(highScoreKey, highScoreString);
-
 });
 
-
-//LS
-//
-const data = JSON.parse(localStorage.getItem('items'))
+const data = JSON.parse(localStorage.getItem("items"));
 
 function endGame() {
-
   clearInterval(timeInterval);
 
   //If timer runs out then hide quiz page and open enter initial page
   var seconds = 120;
   if (secondsLeft === 0) {
-    enterInitial.style.display = "block"
+    enterInitial.style.display = "block";
     quizPage.style.display = "none";
   }
-
-
-
-
-} 
+}
